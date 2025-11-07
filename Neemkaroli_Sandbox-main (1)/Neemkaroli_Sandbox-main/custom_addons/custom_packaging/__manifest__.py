2fc8df9e28948f42309966f1ee277b56fe1524da
Neemkaroliconsulting <paritoshrohatgi@gmail.com>
2025-11-07 17:03:49 +0530

    Add files via upload

---
+++ Neemkaroli_Sandbox-main (1)/Neemkaroli_Sandbox-main/custom_addons/custom_packaging/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Custom Packaging Table',
+    'version': '1.0',
+    'summary': 'Add packaging lines table and CBM/CFT computation',
+    'category': 'Inventory',
+    'author': 'You',
+    'depends': ['product', 'stock'],
+    'data': [
+        'views/product_template_views.xml',
+    ],
+    'installable': True,
+    'application': False,
+}
