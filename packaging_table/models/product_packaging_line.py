77cf7f1d07ec7cba8559c84fc744e1860745f878
Neemkaroliconsulting <paritoshrohatgi@gmail.com>
2025-11-08 12:53:35 +0530

    Add files via upload

--- packaging_table/models/product_packaging_line.py
+++ packaging_table/models/product_packaging_line.py
@@ -4,17 +4,13 @@ class ProductPackagingLine(models.Model):
     _name = 'product.packaging.line'
     _description = 'Product Packaging Line'
 
-    product_tmpl_id = fields.Many2one(
-        'product.template', string='Product', ondelete='cascade'
-    )
-
+    product_tmpl_id = fields.Many2one('product.template', string='Product', ondelete='cascade')
     item_level = fields.Selection([
         ('l1', 'L1 - Poly Pack'),
         ('l2', 'L2 - Inner Box'),
         ('l3', 'L3 - Master Carton'),
         ('l4', 'L4 - Outer Carton'),
     ], string='Item Level')
-
     length = fields.Float('Length (cm)')
     width = fields.Float('Width (cm)')
     height = fields.Float('Height (cm)')
@@ -26,10 +22,7 @@ class ProductPackagingLine(models.Model):
     @api.depends('length', 'width', 'height')
     def _compute_cbm(self):
         for rec in self:
-            if rec.length and rec.width and rec.height:
-                rec.cbm = (rec.length * rec.width * rec.height) / 1000000
-            else:
-                rec.cbm = 0.0
+            rec.cbm = (rec.length * rec.width * rec.height) / 1000000.0 if rec.length and rec.width and rec.height else 0.0
 
     @api.depends('cbm')
     def _compute_cft(self):
