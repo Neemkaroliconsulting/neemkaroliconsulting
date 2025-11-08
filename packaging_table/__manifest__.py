b2bd1b84437e498453ddbb2ee1684c650a8e87b2
Neemkaroliconsulting <paritoshrohatgi@gmail.com>
2025-11-08 15:53:39 +0530

    Add files via upload

--- packaging_table/__manifest__.py
+++ packaging_table/__manifest__.py
@@ -2,7 +2,9 @@
     'name': 'Packaging infoooo Table',
     'version': '1.0',
     'depends': ['product', 'stock'],
-    'data': ['views/product_template_views.xml'],
+    'data': [
+    'views/product_template_views.xml',
+    ],
     'installable': True,
     'application': False,
 }
--- packaging_table/models/__init__.py
+++ packaging_table/models/__init__.py
@@ -1,2 +1,2 @@
-from . import product_packaging
 from . import product_packaging_line
+from . import product_packaging
