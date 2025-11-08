4c8f3dac95b3b07a8ab77d5ac5b1a36607c1fff6
Neemkaroliconsulting <paritoshrohatgi@gmail.com>
2025-11-08 12:40:24 +0530

    Add files via upload

---
+++ packaging_table/__manifest__.py
@@ -0,0 +1,9 @@
+{
+    'name': ' Packaging Table',
+    'version': '1.0',
+    'summary': 'Add Packaging Info table to Product',
+    'depends': ['product', 'stock'],
+    'data': ['views/product_template_views.xml'],
+    'installable': True,
+    'application': False,
+}
---
+++ packaging_table/models/product_packaging_line.py
@@ -0,0 +1,37 @@
+from odoo import models, fields, api
+
+class ProductPackagingLine(models.Model):
+    _name = 'product.packaging.line'
+    _description = 'Product Packaging Line'
+
+    product_tmpl_id = fields.Many2one(
+        'product.template', string='Product', ondelete='cascade'
+    )
+
+    item_level = fields.Selection([
+        ('l1', 'L1 - Poly Pack'),
+        ('l2', 'L2 - Inner Box'),
+        ('l3', 'L3 - Master Carton'),
+        ('l4', 'L4 - Outer Carton'),
+    ], string='Item Level')
+
+    length = fields.Float('Length (cm)')
+    width = fields.Float('Width (cm)')
+    height = fields.Float('Height (cm)')
+    net_weight = fields.Float('Net Wt (kg)')
+    gross_weight = fields.Float('Gross Wt (kg)')
+    cbm = fields.Float('CBM (m³)', compute='_compute_cbm', store=True)
+    cft = fields.Float('CFT', compute='_compute_cft', store=True)
+
+    @api.depends('length', 'width', 'height')
+    def _compute_cbm(self):
+        for rec in self:
+            if rec.length and rec.width and rec.height:
+                rec.cbm = (rec.length * rec.width * rec.height) / 1000000.0
+            else:
+                rec.cbm = 0.0
+
+    @api.depends('cbm')
+    def _compute_cft(self):
+        for rec in self:
+            rec.cft = rec.cbm * 35.315 if rec.cbm else 0.0
