c42f40696d71125b7d57ab46a755d69c5e533e16
Neemkaroliconsulting <paritoshrohatgi@gmail.com>
2025-11-08 15:03:43 +0530

    Add files via upload

---
+++ packaging_table_final/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Packaging Table Final',
+    'version': '1.0',
+    'summary': 'Add multi-record Packaging table in Product form with auto CBM/CFT calculation',
+    'category': 'Inventory',
+    'author': 'ChatGPT for Anshika',
+    'depends': ['product', 'stock'],
+    'data': [
+        'views/product_template_views.xml',
+    ],
+    'installable': True,
+    'application': False,
+}
