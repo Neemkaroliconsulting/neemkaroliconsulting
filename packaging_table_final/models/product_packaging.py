299dc58bde5c7bd973494e87b599b9caa4470a6e
Neemkaroliconsulting <paritoshrohatgi@gmail.com>
2025-11-08 15:08:18 +0530

    Add files via upload

---
+++ packaging_table_final/models/product_packaging.py
@@ -0,0 +1,10 @@
+from odoo import models, fields
+
+class ProductTemplate(models.Model):
+    _inherit = 'product.template'
+
+    packaging_line_ids = fields.One2many(
+        'product.packaging.line',
+        'product_tmpl_id',
+        string='Packaging Lines'
+    )
