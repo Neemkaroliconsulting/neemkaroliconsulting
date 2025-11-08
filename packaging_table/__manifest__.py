2465be717f24011c8a1f65782964764e4fd3dd32
Neemkaroliconsulting <paritoshrohatgi@gmail.com>
2025-11-08 13:33:11 +0530

    all15

--- packaging_table/__manifest__.py
+++ packaging_table/__manifest__.py
@@ -1,7 +1,8 @@
 {
-    'name': 'Packaging Multi inffo Table',
+    'name': 'Packaging infoooo Table',
     'version': '1.0',
     'depends': ['product', 'stock'],
     'data': ['views/product_template_views.xml'],
     'installable': True,
+    'application': False,
 }
