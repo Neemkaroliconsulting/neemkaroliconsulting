679f8ff3fa833a7563b4aa00be846f6e64053477
Neemkaroliconsulting <paritoshrohatgi@gmail.com>
2025-11-08 13:24:02 +0530

    all12

--- packaging_table/__manifest__.py
+++ packaging_table/__manifest__.py
@@ -1,11 +1,8 @@
 {
-    'name': 'Custom Packaging Multi Info',
+    'name': 'Custom Packaging Multi info Table',
     'version': '1.0',
-    'summary': 'Add multi-record Packaging Info table to Product form (auto CBM & CFT)',
+    'summary': 'Add multi-record packaging table on product form',
     'depends': ['product', 'stock'],
-    'data': [
-        'views/product_template_views.xml',
-    ],
+    'data': ['views/product_template_views.xml'],
     'installable': True,
-    'application': False,
 }
