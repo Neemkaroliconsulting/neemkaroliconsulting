95dc3c8f542b14f2261543d116535b8908e622e3
Neemkaroliconsulting <paritoshrohatgi@gmail.com>
2025-11-08 16:44:49 +0530

    Deleted custom_package folder

--- custom_addons/custom_packaging/__manifest__.py
+++
@@ -1,7 +0,0 @@
-{
-    'name': 'Custom Packaging',
-    'version': '1.0',
-    'depends': ['product'],
-    'data': ['views/product_template_views.xml'],
-    'installable': True,
-}
--- custom_addons/custom_packaging/models/__init__.py
+++
@@ -1,2 +0,0 @@
-from . import product_packaging_line
-from . import product_packaging
